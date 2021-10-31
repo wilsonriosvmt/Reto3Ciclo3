6d6bde98b3264aad3bc74097477f24f96dd63db3
Wilson Rios V <91927683+wilsonriosvmt@users.noreply.github.com>
2021-10-31 01:00:49 -0500

    Corrigiendo un error de unauthorized en security
    Corrigiendo un error de unauthorized en security

--- Reto3Ciclo3/src/main/java/security/SecurityAdapter.java
+++ Reto3Ciclo3/src/main/java/security/SecurityAdapter.java
@@ -1,11 +1,39 @@
 package security;
-
+/*
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class SecurityAdapter extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(a -> a
+                .antMatchers("/", "/error", "/webjars/**",
+                        "/api/**").permitAll()
+                .anyRequest().authenticated()
+        ).exceptionHandling(e -> e
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+        ).oauth2Login().defaultSuccessUrl("/", true);
+
+        http.cors().and().csrf().disable();
+
+    }
+
+}
+
+*/
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
 @RestController
 public class SecurityAdapter extends WebSecurityConfigurerAdapter {
     @Override
